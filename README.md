d3852e52a11736dd7e82d664ff24f28fb06a5ee9
akira_cn <akira.cn@gmail.com>
2015-10-16 00:48:54 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,143 @@
+# Think-wechat
+
+微信中间件，同时支持 think.js 1.x 和 2.0，基于 [https://github.com/node-webot/wechat]
+
+## Getting start
+
+```bash
+$ git clone https://github.com/akira-cn/think-wechat.git
+$ cd think-wechat
+$ npm install
+```
+
+## 在 think.js 1.x 中使用
+
+1. 配置支持用户tag，在 App/Conf/config.js 中设置 app_tag_on: true
+
+```js
+module.exports = {
+  //配置项: 配置值
+  port: 8360, //监听的端口
+  app_tag_on: true
+};
+```
+
+2. 编辑 App/Conf/tag.js 文件内容
+
+```js
+/**
+ * 解析提交的json数据
+ * @param  {[type]} http [description]
+ * @return {[type]}      [description]
+ */
+var wechatMiddleware = require('think-wechat'); 
+
+module.exports = {
+  form_parse: [false, wechatMiddleware({
+        wechat:{
+            token: '微信公众号token',
+            appid: '微信公众号ID',
+            encodingAESKey: '消息安全加密串'
+        },
+    })],
+};
+```
+
+3. 增加对应的Controller和Action，默认为WechatController
+
+```js
+module.exports = Controller("Home/BaseController", function(){
+  "use strict";
+  return {
+    textAction: function(){
+      var message = this.post();
+      this.http.res.reply('消息接收成功！');
+    },
+    __call: function(action){
+      var message = this.post();
+      this.http.res.reply(JSON.stringify(message));
+    }
+  };
+});
+```
+
+## 在 think.js 2.0 中使用
+
+1. 编辑 config/hook.js 增加一个 hook
+
+```js
+export default {
+  payload_parse: ['prepend', 'parse_wechat'], //在前面追加解析 xml
+}
+```
+
+2. 编辑 bootstrap/hook.js 加载 hook
+
+```js
+var wechatMiddleware = require('think-wechat');
+
+think.middleware('parse_wechat', wechatMiddleware({
+        wechat:{
+            token: '微信公众号token',
+            appid: '微信公众号ID',
+            encodingAESKey: '消息安全加密串'
+        },
+    }));
+```
+
+3. 增加对应的controller和action，默认为wechat
+
+```js
+'use strict';
+
+import Base from './base.js';
+
+const DEFULT_AUTO_REPLY = '功能正在开发中~';
+
+export default class extends Base {
+  reply(message){
+    this.http.res.reply(message);
+  }
+  /**
+   * index action
+   * @return {Promise} []
+   */
+  textAction(){
+    var message = this.post();
+    var msg = message.Content.trim();
+    this.reply('测试成功:'+msg);
+  }
+  eventAction(){
+    var message = this.post();
+    this.reply(JSON.stringify(message));
+  }
+  __call(){
+    this.reply(DEFULT_AUTO_REPLY);
+  }
+}
+```
+
+## 高级配置
+
+```js
+var wechatMiddleware = require('think-wechat');
+
+think.middleware('parse_wechat', wechatMiddleware({
+        pathname: '/wechat',    //默认，可配置为其他路径，与公众号对应的服务器URL设置一致
+        route: {
+          text: '/wechat/text', //文字转发
+          image: '/wechat/image', //图片转发
+          voice: '/wechat/voice', //语音转发
+          video: '/wechat/video', //视频转发
+          shortvideo: '/wechat/shortvideo', //小视频转发
+          location: '/wechat/location', //地理位置转发
+          link: '/wechat/link', //链接转发
+          event: '/wechat/event', //推送事件转发
+        },
+        wechat:{
+            token: '微信公众号token',
+            appid: '微信公众号ID',
+            encodingAESKey: '消息安全加密串'
+        },
+    }));
+```
---
+++ index.js
@@ -0,0 +1,146 @@
+module.exports = function(conf){
+  return function(http){
+    var self = this;
+
+    return new Promise(function(resolve, reject){
+      var connect = require('http-connect');
+      var app = new connect(http);
+      var wechat = require('wechat');
+
+      app.use('/wechat', function(req, res, next){
+          req.rawBody = http.payload;
+          next();
+      });
+
+      function forward(path, message){
+          if(typeof http.post === 'function'){
+            http._post = message;
+          }else{
+            http.post = message;
+          }
+          http.pathname += '/' + path;
+          resolve();
+      }
+
+      app.use('/wechat', wechat(conf.wechat)
+        .text(function (message, req, res, next) {
+          // message为文本内容
+          // { ToUserName: 'gh_d3e07d51b513',
+          // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
+          // CreateTime: '1359125035',
+          // MsgType: 'text',
+          // Content: 'http',
+          // MsgId: '5837397576500011341' }
+          forward('text', message);
+        }).image(function (message, req, res, next) {
+          // message为图片内容
+          // { ToUserName: 'gh_d3e07d51b513',
+          // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
+          // CreateTime: '1359124971',
+          // MsgType: 'image',
+          // PicUrl: 'http://mmsns.qpic.cn/mmsns/bfc815ygvIWcaaZlEXJV7NzhmA3Y2fc4eBOxLjpPI60Q1Q6ibYicwg/0',
+          // MediaId: 'media_id',
+          // MsgId: '5837397301622104395' }
+          forward('image', message);
+        }).voice(function (message, req, res, next) {
+          // message为音频内容
+          // { ToUserName: 'gh_d3e07d51b513',
+          // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
+          // CreateTime: '1359125022',
+          // MsgType: 'voice',
+          // MediaId: 'OMYnpghh8fRfzHL8obuboDN9rmLig4s0xdpoNT6a5BoFZWufbE6srbCKc_bxduzS',
+          // Format: 'amr',
+          // MsgId: '5837397520665436492' }
+          forward('voice', message);
+        }).video(function (message, req, res, next) {
+          // message为视频内容
+          // { ToUserName: 'gh_d3e07d51b513',
+          // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
+          // CreateTime: '1359125022',
+          // MsgType: 'video',
+          // MediaId: 'OMYnpghh8fRfzHL8obuboDN9rmLig4s0xdpoNT6a5BoFZWufbE6srbCKc_bxduzS',
+          // ThumbMediaId: 'media_id',
+          // MsgId: '5837397520665436492' }
+          forward('video', message);
+        }).shortvideo(function (message, req, res, next) {
+          // message为短视频内容
+          // { ToUserName: 'gh_d3e07d51b513',
+          // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
+          // CreateTime: '1359125022',
+          // MsgType: 'shortvideo',
+          // MediaId: 'OMYnpghh8fRfzHL8obuboDN9rmLig4s0xdpoNT6a5BoFZWufbE6srbCKc_bxduzS',
+          // ThumbMediaId: 'media_id',
+          // MsgId: '5837397520665436492' }
+          forward('shortvideo', message);
+        }).location(function (message, req, res, next) {
+          // message为位置内容
+          // { ToUserName: 'gh_d3e07d51b513',
+          // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
+          // CreateTime: '1359125311',
+          // MsgType: 'location',
+          // Location_X: '30.283950',
+          // Location_Y: '120.063139',
+          // Scale: '15',
+          // Label: {},
+          // MsgId: '5837398761910985062' }
+          forward('location', message);
+        }).link(function (message, req, res, next) {
+          // message为链接内容
+          // { ToUserName: 'gh_d3e07d51b513',
+          // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
+          // CreateTime: '1359125022',
+          // MsgType: 'link',
+          // Title: '公众平台官网链接',
+          // Description: '公众平台官网链接',
+          // Url: 'http://1024.com/',
+          // MsgId: '5837397520665436492' }
+          forward('link', message);
+        }).event(function (message, req, res, next) {
+          // message为事件内容
+          // { ToUserName: 'gh_d3e07d51b513',
+          // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
+          // CreateTime: '1359125022',
+          // MsgType: 'event',
+          // Event: 'LOCATION',
+          // Latitude: '23.137466',
+          // Longitude: '113.352425',
+          // Precision: '119.385040',
+          // MsgId: '5837397520665436492' }
+          forward('event', message);
+        }).device_text(function (message, req, res, next) {
+          // message为设备文本消息内容
+          // { ToUserName: 'gh_d3e07d51b513',
+          // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
+          // CreateTime: '1359125022',
+          // MsgType: 'device_text',
+          // DeviceType: 'gh_d3e07d51b513'
+          // DeviceID: 'dev1234abcd',
+          // Content: 'd2hvc3lvdXJkYWRkeQ==',
+          // SessionID: '9394',
+          // MsgId: '5837397520665436492',
+          // OpenID: 'oPKu7jgOibOA-De4u8J2RuNKpZRw' }
+          forward('deviceText', message);
+        }).device_event(function (message, req, res, next) {
+          // message为设备事件内容
+          // { ToUserName: 'gh_d3e07d51b513',
+          // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
+          // CreateTime: '1359125022',
+          // MsgType: 'device_event',
+          // Event: 'bind'
+          // DeviceType: 'gh_d3e07d51b513'
+          // DeviceID: 'dev1234abcd',
+          // OpType : 0, //Event为subscribe_status/unsubscribe_status时存在
+          // Content: 'd2hvc3lvdXJkYWRkeQ==', //Event不为subscribe_status/unsubscribe_status时存在
+          // SessionID: '9394',
+          // MsgId: '5837397520665436492',
+          // OpenID: 'oPKu7jgOibOA-De4u8J2RuNKpZRw' }
+          forward('deviceEvent', message);
+        })
+        .middlewarify());
+
+        app.use(function(){
+          resolve();
+        });     
+    });
+  }
+}
